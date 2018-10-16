8cc27cce46b372e0962e53d80d0fc310ec6376f2
Chestirito <19494641+Chestirito@users.noreply.github.com>
2018-10-16 13:15:30 -0400

    Updates

---
+++ precache-manifest.fdacdacf3169b29e5309c5e0a4cd824e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "597238e426f8957b3cff",
+    "url": "/static/js/main.597238e4.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "597238e426f8957b3cff",
+    "url": "/static/css/main.cac0f862.chunk.css"
+  },
+  {
+    "revision": "be7fccf809a3d2399ddd63ba5f23a587",
+    "url": "/index.html"
+  }
+];
